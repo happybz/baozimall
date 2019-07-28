b8e06a1465e83bdbab313d7ee4201b1977d562fd
Happy_Baozi <happy_bz@qq.com>
2019-07-28 20:27:07 +0800

    init pojo & mapping by generator

---
+++ src/main/java/com/baozimall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.baozimall.dao;
+
+import com.baozimall.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
---
+++ src/main/java/com/baozimall/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.baozimall.dao;
+
+import com.baozimall.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
