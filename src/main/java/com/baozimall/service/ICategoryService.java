d6949a90218b8143d84751f23f08af77bfb3fcb9
Happy_Baozi <happy_bz@qq.com>
2019-07-29 09:36:10 +0800

    分类管理模块开发

---
+++ src/main/java/com/baozimall/service/ICategoryService.java
@@ -0,0 +1,16 @@
+package com.baozimall.service;
+
+import com.baozimall.common.ServerResponse;
+import com.baozimall.pojo.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    ServerResponse addCategory(String categoryName, Integer parentId);
+
+    ServerResponse updateCategoryName(Integer categoryId, String categoryName);
+
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
+    ServerResponse<List<Integer>> selectCategoryAndChildrenById(Integer categoryId);
+}
