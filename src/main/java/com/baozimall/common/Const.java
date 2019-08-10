838710568e1447fbc9b85262bc5404ed6cd6b1f4
Happy_Baozi <happy_bz@qq.com>
2019-08-10 20:40:20 +0800

    Spring Schedule 4种方式实现定时关单

--- src/main/java/com/baozimall/common/Const.java
+++ src/main/java/com/baozimall/common/Const.java
@@ -146,4 +146,8 @@ public class Const {
     public interface RedisCacheExtime{
         int REDIS_SESSION_EXTIME = 60 * 30;//30分钟
     }
+
+    public interface REDIS_LOCK{
+        String CLOSE_ORDER_TASK_LOCK = "CLOSE_ORDER_TASK_LOCK";//关闭订单的分布式锁
+    }
 }
